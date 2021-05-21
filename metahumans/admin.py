24c8990ebdf0af6c7a980ad020cddc1d92c93811
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-05-21 14:36:13 +0100

    Añade admin. Dos nuevos superheroes en el CSV

--- metahumans/admin.py
+++ metahumans/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Equipo, Poder, Metahumano
+
+class PoderAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Poder, PoderAdmin)
+
+class EquipoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Equipo, EquipoAdmin)
+
+class MetahumanoAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Metahumano, MetahumanoAdmin)
+
