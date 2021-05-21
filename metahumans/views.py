a40c395dd7d709363bb8f59fb0185d4a0e4688e1
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-05-21 13:12:39 +0100

    Nueva vista para mostrar los metahumans, con su nivel de amenaza, a partir de un fichero CSV

--- metahumans/views.py
+++ metahumans/views.py
@@ -1,3 +1,22 @@
+import csv
+
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def listado_metahumans(request):
+    with open("superheroes.csv", encoding="utf-8") as f:
+        r = csv.reader(f, delimiter=";")
+        next(r)
+        filas = [
+            {
+                "nombre": tupla[0],
+                "nivel": int(tupla[2]),
+                "peligroso": int(tupla[2]) >= 50,
+            }
+            for tupla in r
+            ]
+        return render(request, 'metahumans/listado.html', {
+            "filas": filas
+        })
