e8829839e22eb877b4d99183a9cbe9ee71a95c6a
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-05-26 01:54:36 +0100

    Añadimos bootstrap para mejorar la presentación

--- metahumans/views.py
+++ metahumans/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
 from .models import Metahumano
+
 # Create your views here.
+
+def detalle(request, pk):
+    metahuman = Metahumano.objects.get(id=pk)
+    return render(request, 'metahumans/detalle.html', {
+        "mh": metahuman, 
+    })
 
 
 def listado_metahumans(request):
--- shield/urls.py
+++ shield/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 import metahumans.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listado/', metahumans.views.listado_metahumans),
+    path('metahumano/<int:pk>/', metahumans.views.detalle),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
